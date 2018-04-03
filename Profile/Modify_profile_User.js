aac02e4a4f6278491d3c5a00f5b6cb80c78d7fe2
RandyMurillo <37453026+RandyMurillo@users.noreply.github.com>
2018-04-03 14:44:26 -0600

    Add files via upload

---
+++ Profile/Modify_profile_User.js
@@ -0,0 +1,13 @@
+if (typeof (dojo) != "undefined") {
+require(["dojo/domReady!"], function () {
+try {
+dojo.place(
+"<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/profiles/profile_modified_rmm.css?repoName=helloworld-cv3-rmm\"></link>",
+dojo.doc.head,
+'last'
+);
+} catch (e) {
+alert('exception occurred : ' + e);
+}
+});
+}
