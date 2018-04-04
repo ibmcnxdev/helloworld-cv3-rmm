1bff681754bcc9ac11cd1247ff71d66d12496c68
RandyMurillo <37453026+RandyMurillo@users.noreply.github.com>
2018-04-04 10:35:52 -0600

    Update Modify_profile_User.js

--- Profile/Modify_profile_User.js
+++ Profile/Modify_profile_User.js
@@ -2,9 +2,9 @@ if (typeof (dojo) != "undefined") {
 require(["dojo/domReady!"], function () {
 try {
 dojo.place(
-"<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/profiles/profile_modified_rmm.css?repoName=helloworld-cv3-rmm\"></link>",
+"<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/Profile/profile_modified_rmm.css?repoName=helloworld-cv3-rmm\"></link>",
 dojo.doc.head,
-'last'
+"last"
 );
 } catch (e) {
 alert('exception occurred : ' + e);
